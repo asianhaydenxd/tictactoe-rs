258c80d4b287ebfede3959f8647d6ece9c060f6d
Hayden <asianhaydenxd@gmail.com>
2022-09-18 22:17:58 -0400

    Implement turns

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,31 @@ enum Cell {
     O,
 }
 
-type Board = [[Option<Cell>; 3]; 3];
+struct Game { 
+    board: [[Option<Cell>; 3]; 3],
+    turn: Cell,
+}
 
 fn draw_grid_cell(x: usize, y: usize, cell: Option<Cell>) {
     draw_rectangle(
-        screen_width() / 2.0 - 150.0 + (100.0 * x as f32), 
-        screen_height() / 2.0 - 150.0 + (100.0 * y as f32), 
-        100.0, 100.0, 
+        screen_width() / 2.0 - 145.0 + (100.0 * x as f32), 
+        screen_height() / 2.0 - 145.0 + (100.0 * y as f32), 
+        90.0, 90.0, 
         match cell {
-            None => LIGHTGRAY,
+            None => GRAY,
             Some(Cell::X) => RED,
             Some(Cell::O) => BLUE,
         }
     );
 }
 
-fn render_game(board: &Board) {
-    for (x, row) in board.iter().enumerate() {
+fn render_game(game: &Game) {
+    draw_rectangle(screen_width() / 2.0 - 25.0, screen_height() / 2.0 - 250.0, 50.0, 50.0, match game.turn {
+        Cell::X => RED,
+        Cell::O => BLUE,
+    });
+
+    for (x, row) in game.board.iter().enumerate() {
         for (y, cell) in row.iter().enumerate() {
             draw_grid_cell(x, y, *cell);
         }
@@ -31,11 +39,14 @@ fn render_game(board: &Board) {
 
 #[macroquad::main("TicTacToe")]
 async fn main() {
-    let mut board: Board = [[None; 3]; 3];
+    let mut game: Game = Game {
+        board: [[None; 3]; 3],
+        turn: Cell::X,
+    };
 
     loop {
         clear_background(WHITE);
-        render_game(&board);
+        render_game(&game);
         next_frame().await;
     }
 }

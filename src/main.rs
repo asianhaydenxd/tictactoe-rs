e4a43bd8a17706585bd175c974e63be3344f11aa
Hayden <asianhaydenxd@gmail.com>
2022-09-18 22:54:53 -0400

    Add mouse input functionality

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,40 @@ fn render_game(game: &Game) {
     }
 }
 
+fn is_coord_over_square(total: f32, mouse_coord: f32, level: usize) -> bool {
+    total / 2.0 - 145.0 + (100.0 * level as f32) < mouse_coord
+        && mouse_coord < total / 2.0 - 145.0 + (100.0 * level as f32) + 90.0
+}
+
+fn get_mouse_cell_coords(mouse_position: (f32, f32)) -> Option<(usize, usize)> {
+    let (x, y) = mouse_position;
+
+    let xsize: usize;
+    let ysize: usize;
+
+    if is_coord_over_square(screen_width(), x, 0) {
+        xsize = 0;
+    } else if is_coord_over_square(screen_width(), x, 1) {
+        xsize = 1;
+    } else if is_coord_over_square(screen_width(), x, 2) {
+        xsize = 2;
+    } else {
+        return None;
+    }
+
+    if is_coord_over_square(screen_height(), y, 0) {
+        ysize = 0;
+    } else if is_coord_over_square(screen_height(), y, 1) {
+        ysize = 1;
+    } else if is_coord_over_square(screen_height(), y, 2) {
+        ysize = 2;
+    } else {
+        return None;
+    }
+
+    Some((xsize, ysize))
+}
+
 #[macroquad::main("TicTacToe")]
 async fn main() {
     let mut game: Game = Game {
@@ -45,6 +79,20 @@ async fn main() {
     };
 
     loop {
+        let mouse_cell_coords_result = get_mouse_cell_coords(mouse_position());
+        if is_mouse_button_pressed(MouseButton::Left) {
+            match mouse_cell_coords_result {
+                Some((x, y)) => {
+                    game.board[x][y] = Some(game.turn);
+                    game.turn = match game.turn {
+                        Cell::X => Cell::O,
+                        Cell::O => Cell::X,
+                    };
+                },
+                None => (),
+            }
+        }
+
         clear_background(WHITE);
         render_game(&game);
         next_frame().await;
